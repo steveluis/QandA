6a3f0139cb377d767d793b6202120334299d2aee
Steve Luis <steveluis@gmail.com>
2015-02-19 21:40:02 -0500

    Initial Commit
    Build a simple question/answer app refactoring different elements to
    test Swift syntax and style.

---
+++ QandA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QandA
+//
+//  Created by Steven Luis on 2/19/15.
+//  Copyright (c) 2015 FIU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
